35e2384af50815f557b0eeea8cbae6cbed5a27d7
dharmendra kumar <dk8430996231@gmail.com>
2020-11-08 23:42:42 +0530

    form jss file `

---
+++ form.js
@@ -0,0 +1,15 @@
+(function( $ ){
+$(document).ready(function(){
+    console.log("dharemendra");
+    //   $("#submit").click(function(){
+    //       console.log("dharmendra");
+    //       alert("dharniououi");
+    // //   $("#Fullnmae").html('<b>' + $("#fname,").val() + '</b>');
+    //   //   $("#email").html('<b>' + $(this).val() + '</b>');
+    //   //   $("#password").html('<b>' + $(this).val() + '</b>');
+       
+
+  
+//       });
+    });
+});
